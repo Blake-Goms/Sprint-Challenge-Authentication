fe3bad65be73dd7c755cadf719c7cb9b0081df9f
Blake Goms <blakegoms@gmail.com>
2019-08-23 08:47:04 -0700

    CRUD done, WIP:testing

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const Users = require('../auth/users-model');
 
 const server = express();
 
@@ -14,5 +15,13 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+server.get('/api/users', authenticate, (req, res) => {
+    Users.find()
+    .then(users => {
+        res.json({ loggedInUser: req.user.username, users });
+    })
+    .catch(err => res.send(err));
+});
 
 module.exports = server;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,26 @@
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+const jwt = require('jsonwebtoken')
+
+const secrets = require('../config/secrets')
+
+module.exports = function restricted(req, res, next) {
+  console.log(req.headers)
+  const token = req.headers.token;
+  if(token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if(err){
+        // token is invalid
+        console.log(err)
+        res.status(401).json({message: 'invalid token'})
+      } else {
+        // good token
+        req.user = { username: decodedToken.username}
+        next();
+      }
+    })
+  } else {
+    res.status(400).json({message: 'no token. gimme token'})
+  }
 };
